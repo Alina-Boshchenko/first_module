cf5c1111bebf2d7be8ed20b6802c8a6107c98b96
Алина Бощенко <166435203+Alina-Boshchenko@users.noreply.github.com>
2025-03-07 08:42:03 +0300

    Update App.java
    исправление явного приведения, добавление параметризации фильтра

--- src/main/java/ru/boshchenko/App.java
+++ src/main/java/ru/boshchenko/App.java
@@ -3,13 +3,11 @@ package ru.boshchenko;
 import java.util.Arrays;
 
 public class App {
-    public static void main(String[] args) {
-    }
-
-    public static <T> T[] filter(T[] array, Filter filter) {
+    
+   public static <T> T[] filter(T[] array, Filter<T> filter) {
         T[] filteredArray = Arrays.copyOf(array, array.length);
         for (int i = 0; i < array.length; i++)
-            filteredArray[i] = (T) filter.apply(array[i]);
+            filteredArray[i] = filter.apply(array[i]);
         return filteredArray;
     }
 }
