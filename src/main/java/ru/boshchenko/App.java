6d09a9700a17d3cfd829b83a79ea1009c1bb159f
Алина Бощенко <166435203+Alina-Boshchenko@users.noreply.github.com>
2025-03-07 08:43:22 +0300

    Update App.java

--- src/main/java/ru/boshchenko/App.java
+++ src/main/java/ru/boshchenko/App.java
@@ -4,9 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) {
-    }
-
+ 
     public static <T> Map<T, Integer> countOfElements(T[] elements) {
         Map<T, Integer> countMap = new HashMap<>();
         for (int i = 0; i < elements.length; i++)
