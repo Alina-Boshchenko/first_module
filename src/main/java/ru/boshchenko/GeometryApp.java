0b5af1ffd367e347107c401b536bc6cc343278a5
AlinaBoschenko <alina.xalimova14@gmail.com>
2025-03-13 01:40:35 +0300

    version_1.1.0

--- src/main/java/ru/boshchenko/GeometryApp.java
+++ src/main/java/ru/boshchenko/GeometryApp.java
@@ -1,26 +1,34 @@
 package ru.boshchenko;
 
-import ru.boshchenko.geometryLibrary.base.twoDimensionalShapes.*;
+import geometryLibrary.base.twoDimensionalShapes.*;
+import geometryUtils.base.MetricSystem;
 
 import java.util.List;
 
 public class GeometryApp {
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(5.0, 3.0, 6.0);
-        Circle circle = new Circle(6.25);
-        Square square = new Square(7.5);
-        Rectangle rectangle = new Rectangle(3.5, 8.4);
+        Triangle triangle = new Triangle(50, 30, 60);
+        Circle circle = new Circle(625);
+        Square square = new Square(75);
+        Rectangle rectangle = new Rectangle(35, 84);
 
         List<TwoShape> twoShapes = List.of(triangle, circle, square, rectangle);
 
-        twoShapes.forEach(shape -> System.out.printf("Фигура: %s\nПериметр = %f\nПлощадь = %f\n\n"
+        twoShapes.forEach(shape -> System.out.printf("Фигура: %s\nПериметр = %d\nПлощадь = %d\n\n"
                 , shape, shape.calculatePerimeter(), shape.calculateArea()));
 
         System.out.println(triangle.getLengthFirstSide() + " " + triangle.getLengthSecondSide() + " " + triangle.getLengthThirdSide());
         System.out.println(circle.getRadius());
         System.out.println(square.getLengthSide());
         System.out.println(rectangle.getLength() + " " + rectangle.getWidth());
+
+        System.out.println("--------------------------------Площадь круга в дргуих метрических значенях -------------------------------------");
+        System.out.println(circle.calculateArea() + " миллиметров");
+        System.out.println(MetricSystem.MILLIMETER.toCentimeters(circle.calculateArea()) + " сантиметров");
+        System.out.println(MetricSystem.MILLIMETER.toDecimeters(circle.calculateArea()) + " дециметров");
+        System.out.println(MetricSystem.MILLIMETER.toMeters(circle.calculateArea()) + " метров");
+        System.out.println(MetricSystem.MILLIMETER.toKilometers(circle.calculateArea()) + " километров");
 
     }
 }
